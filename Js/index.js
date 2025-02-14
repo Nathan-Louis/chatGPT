9f552f80e546807ddb4790a02fe818f7d4b4050c
Nathan Louis <louis.nathan92@gmail.com>
2025-02-15 00:55:33 +0100

    Améliorations

--- Js/index.js
+++ Js/index.js
@@ -2,4 +2,12 @@ function changeBackground() {
   const colors = ["#ffcccb", "#b0e57c", "#ffb347", "#8ecae6", "#f4a261"];
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
   document.body.style.backgroundColor = randomColor;
+
+  let colorText = document.getElementById("colorText");
+  if (!colorText) {
+    colorText = document.createElement("p");
+    colorText.id = "colorText";
+    document.body.appendChild(colorText);
+  }
+  colorText.innerText = `Couleur actuelle : ${randomColor}`;
 }
