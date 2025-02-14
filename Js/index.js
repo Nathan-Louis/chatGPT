84859b33f50984f9387a209a01e45c86689a00c6
Nathan Louis <louis.nathan92@gmail.com>
2025-02-15 00:42:31 +0100

    Amélioration du site

--- Js/index.js
+++ Js/index.js
@@ -1,9 +1,5 @@
-<button onclick="changeBackground()">Changer la couleur</button>
-
-<script>
 function changeBackground() {
-    const colors = ["#ffcccb", "#b0e57c", "#ffb347", "#8ecae6", "#f4a261"];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
+  const colors = ["#ffcccb", "#b0e57c", "#ffb347", "#8ecae6", "#f4a261"];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
 }
-</script>
