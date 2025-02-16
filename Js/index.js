3e2f72fe20140ec2473bc213b1eee879b51a0da3
Nathan Louis <louis.nathan92@gmail.com>
2025-02-16 01:00:39 +0100

    Ajout fonction generateur citation

--- Js/index.js
+++ Js/index.js
@@ -22,3 +22,27 @@ let btnColor = document.getElementById("changeColorBtn");
 
 // Ajouter un événement "click" pour changer la couleur
 btnColor.addEventListener("click", changeColor);
+
+// CITATIONS
+
+const quotes = [
+  "La seule façon de faire du bon travail est d’aimer ce que vous faites. – Steve Jobs",
+  "Le succès, c'est tomber sept fois et se relever huit. – Proverbe japonais",
+  "L'échec est simplement l'opportunité de recommencer, cette fois plus intelligemment. – Henry Ford",
+  "N’abandonnez jamais un rêve juste à cause du temps qu’il faudra pour l’accomplir. Le temps passera de toute façon. – Earl Nightingale",
+  "Tout semble impossible jusqu’à ce qu’on le fasse. – Nelson Mandela",
+];
+
+function changeQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+  document.getElementById("quoteText").innerText = quotes[randomIndex];
+  document.getElementById("quoteText").style.color = randomColor;
+}
+
+let quoteBtn = document.getElementById("newQuoteBtn");
+
+quoteBtn.addEventListener("click", () => {
+  changeQuote();
+});
